f1356f691b51942b3ee22f107093c14a80b7b9ef
EpicPuppy <epicpuppy@epicpuppy.dev>
2026-04-13 10:36:39 -0700

    Setup dependencies

--- src/client/java/dev/epicpuppy/wynnpelago/client/WynnpelagoClient.java
+++ src/client/java/dev/epicpuppy/wynnpelago/client/WynnpelagoClient.java
@@ -1,10 +1,14 @@
 package dev.epicpuppy.wynnpelago.client;
 
+import dev.epicpuppy.wynnpelago.client.unlock.RegionUnlock;
 import net.fabricmc.api.ClientModInitializer;
 
 public class WynnpelagoClient implements ClientModInitializer {
+	private RegionUnlock regionLockService;
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		regionLockService = new RegionUnlock();
 	}
 }
---
+++ src/client/java/dev/epicpuppy/wynnpelago/client/unlock/RegionUnlock.java
@@ -0,0 +1,50 @@
+package dev.epicpuppy.wynnpelago.client.unlock;
+
+import com.wynntils.core.components.Models;
+import com.wynntils.models.territories.profile.TerritoryProfile;
+import com.wynntils.utils.mc.McUtils;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.Minecraft;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RegionUnlock {
+    private static List<String> RESPAWN_TERRITORIES = List.of(
+            "Ragni",
+            "Alekin",
+            "Detlas",
+            "Tempo Town",
+            "Maltic",
+            "Ternaves",
+            "Elkurn",
+            "Nemract",
+            "Bremminglar",
+            "Selchar",
+            "Ragni Main Entrance"
+    );
+
+    private Set<String> unlockedTerritories;
+    private int cooldownTicks;
+
+    public RegionUnlock() {
+        unlockedTerritories = new HashSet<>();
+        unlockedTerritories.addAll(RESPAWN_TERRITORIES);
+
+        cooldownTicks = 200;
+
+        ClientTickEvents.END_CLIENT_TICK.register(this::onEndTick);
+    }
+
+    private void onEndTick(Minecraft client) {
+        if (client.player == null) return;
+        if (--cooldownTicks > 0) return;
+        TerritoryProfile territory = Models.Territory.getTerritoryProfileForPosition(client.player.position());
+        if (territory == null) return;
+        if (!unlockedTerritories.contains(territory.getName())) {
+            McUtils.sendChat("/kill");
+            cooldownTicks = 200;
+        }
+    }
+}
