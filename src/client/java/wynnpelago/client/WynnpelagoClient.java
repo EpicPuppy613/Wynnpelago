1e69ab22dac8f31ff74cd0b7b7ef1bf9c5fc2ca4
EpicPuppy <epicpuppy@epicpuppy.dev>
2026-04-12 20:39:37 -0700

    Initial Commit

---
+++ src/client/java/wynnpelago/client/WynnpelagoClient.java
@@ -0,0 +1,10 @@
+package wynnpelago.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WynnpelagoClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
