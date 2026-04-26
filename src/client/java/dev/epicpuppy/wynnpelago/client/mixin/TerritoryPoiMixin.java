2daa7a6dcc2d68f7a0d4cd85bc03fcc827fe9e93
EpicPuppy <epicpuppy@epicpuppy.dev>
2026-04-26 13:09:32 -0700

    Force guild map to show territory abbreviations

--- src/client/java/dev/epicpuppy/wynnpelago/client/mixin/TerritoryPoiMixin.java
+++ src/client/java/dev/epicpuppy/wynnpelago/client/mixin/TerritoryPoiMixin.java
@@ -1,12 +1,17 @@
 package dev.epicpuppy.wynnpelago.client.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.wynntils.models.territories.TerritoryInfo;
 import com.wynntils.models.territories.profile.TerritoryProfile;
 import com.wynntils.services.map.pois.TerritoryPoi;
 import com.wynntils.utils.colors.CommonColors;
 import com.wynntils.utils.colors.CustomColor;
 import dev.epicpuppy.wynnpelago.client.WynnpelagoClient;
 import dev.epicpuppy.wynnpelago.client.unlock.TerritoryUnlock;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -24,5 +29,22 @@ public class TerritoryPoiMixin {
         } else {
             return List.of(CommonColors.RED);
         }
+    }
+
+    @WrapOperation(
+            method = "renderAt",
+            at = @At(value = "INVOKE", target = "Lcom/wynntils/models/territories/TerritoryInfo;isHeadquarters()Z"))
+    private boolean disableHeadquarters(TerritoryInfo instance, Operation<Boolean> original) {
+        if (!WynnpelagoClient.enabled) return original.call(instance);
+        return false;
+    }
+
+    @ModifyVariable(method = "renderAt", at = @At(value = "LOAD", ordinal = 0), name = "mapText")
+    private String changeMapText(String mapText) {
+        if (!WynnpelagoClient.enabled) return mapText;
+        TerritoryProfile territory = ((TerritoryPoiAccessor) this).wynnpelago$getTerritoryProfile();
+        return Arrays.stream(territory.getName().split(" "))
+                .map((s) -> s.substring(0, 1))
+                .collect(Collectors.joining());
     }
 }
